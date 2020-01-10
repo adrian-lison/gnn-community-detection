cec5f4214e8567d16b965cb80c93f94a7171db28
Adrian Lison <a_liso02@uni-muenster.de>
2020-01-10 15:46:33 +0100

    move batchnorm to gcn layer

--- Notebooks/GCN.py
+++ Notebooks/GCN.py
@@ -19,18 +19,13 @@ import numpy as np
 class LinearModule(nn.Module):
     """Linear transformation part of the GCN layer"""
 
-    def __init__(self, in_feats, out_feats, activation, batchnorm=False):
+    def __init__(self, in_feats, out_feats, activation):
         super(LinearModule, self).__init__()
-        self.batchnorm = batchnorm
-        self.bn = nn.BatchNorm1d(in_feats)
         self.linear = nn.Linear(in_feats, out_feats)
         self.activation = activation  # This is the activation function
 
     def forward(self, node):
-        if self.batchnorm:
-            h = self.bn(node.data["h"])
-        else:
-            h = node.data["h"]
+        h = node.data["h"]
         h = self.linear(h)
         h = self.activation(h)
         return {"h": h}
@@ -41,10 +36,16 @@ class GCN(nn.Module):
 
     def __init__(self, in_feats, out_feats, activation, batchnorm=False):
         super(GCN, self).__init__()
-        self.apply_mod = LinearModule(in_feats, out_feats, activation, batchnorm)
+        self.apply_mod = LinearModule(in_feats, out_feats, activation)
+        self.batchnorm = batchnorm
+        self.bn = nn.BatchNorm1d(in_feats)
 
     def forward(self, g, feature):
-        g.ndata["h"] = feature
+        if self.batchnorm:
+            g.ndata["h"] = self.bn(feature)
+        else:
+            g.ndata["h"] = feature
+
         g.update_all(
             message_func=fn.copy_src(src="h", out="m"), reduce_func=fn.sum(msg="m", out="h")
         )
